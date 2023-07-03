ee7f7d7f112ab2c16a62ef630d904d001a0c128e
LuPow <68886157+rakky33@users.noreply.github.com>
2023-07-03 14:50:08 +0700

    Update RC_Car.ino

--- RC_Car.ino
+++ RC_Car.ino
@@ -21,16 +21,23 @@
 #define PinBSteering 15
 
 //Variable
-int ch1Value;
-int ch2Value;
-int ch3Value;
-int ch4Value; 
+int RXValue;
+int RYValue;
+int LXValue;
+int LYValue; 
 
 //function
 int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue){
   int ch = pulseIn(channelInput, HIGH, 30000);
   if (ch < 100) return defaultValue;
   return map(ch, 1000, 2000, minLimit, maxLimit);
+}
+
+// Read the switch channel and return a boolean value
+bool readSwitch(byte channelInput, bool defaultValue){
+  int intDefaultValue = (defaultValue)? 100: 0;
+  int ch = readChannel(channelInput, 0, 100, intDefaultValue);
+  return (ch > 50);
 }
 
 //Start run
@@ -51,35 +58,35 @@ void setup() {
 
 void loop() {
   // get value of each channel
-  ch1Value = readChannel(CH1, -100, 100, 0);
-  ch2Value = readChannel(CH2, -100, 100, 0);
-  ch3Value = readChannel(CH3, -100, 100, -10  0);
-  ch4Value = readChannel(CH4, -100, 100, 0);
+  RXValue = readChannel(CH1, -100, 100, 0);
+  RYValue = readChannel(CH2, -100, 100, 0);
+  LXValue = readChannel(CH3, -100, 100, 0);
+  LYValue = readChannel(CH4, -100, 100, 0);
 
   //Forward/Backward
   //////////////////////////////////////////////////////////////////////
 
   //we will use +- 10 as center 
   //If more than 10 then user wanna move robot forward
-  if (ch2Value > 10){
+  if (RYValue > 10){
     //drive motor forward with mapping value for PWM
-    int PWMValue = map(abs(ch2Value), 0, 100, 0, 255);
+    int PWMValue = map(abs(RYValue), 0, 100, 0, 255);
     analogWrite(PWMPin, PWMValue);
     digitalWrite(PinADrive, HIGH);
-    digitalWrite(PINBDrive, LOW);
+    digitalWrite(PinBDrive, LOW);
   }
   //If less than -10 then user wanna move robot backward
-  else if(ch2Value < -10){
+  else if(RYValue < -10){
     //drive motor backward with mapping value for PWM
-    int PWMValue = map(abs(ch2Value), 0, 100, 0, 255);
+    int PWMValue = map(abs(RYValue), 0, 100, 0, 255);
     analogWrite(PWMPin, PWMValue);
     digitalWrite(PinADrive, LOW);
-    digitalWrite(PINBDrive, HIGH);
+    digitalWrite(PinBDrive, HIGH);
   }
   //else if value is between 10 and -10 then we will stop the motor
   else{
     digitalWrite(PinADrive, LOW);
-    digitalWrite(PINBDrive, LOW);
+    digitalWrite(PinBDrive, LOW);
   }
   //////////////////////////////////////////////////////////////////////
 
@@ -88,21 +95,21 @@ void loop() {
 
   //we will use +- 10 as center
   //If more than 10 then user wanna turn right
-  if (ch1Value > 10){
+  if (RXValue > 10){
     //drive motor to turn right
     digitalWrite(PinASteering, HIGH);
-    digitalWrite(PINBSteering, LOW);
+    digitalWrite(PinBSteering, LOW);
   }
   //If less than -10 then user wanna move robot backward
-  else if(ch1Value < -10){
+  else if(RXValue < -10){
     //drive motor to turn left
     digitalWrite(PinASteering, LOW);
-    digitalWrite(PINBSteering, HIGH);
+    digitalWrite(PinBSteering, HIGH);
   }
   //else if value is between 10 and -10 then we will stop the motor
   else{
     digitalWrite(PinASteering, LOW);
-    digitalWrite(PINBSteering, LOW);
+    digitalWrite(PinBSteering, LOW);
   }
   //////////////////////////////////////////////////////////////////////
 
